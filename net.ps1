74e803b60ba62a547fb1b5dea7b195903ca93152
zhongtongzhuo <zhongtongzhuo@126.com>
2019-09-07 14:51:33 +0800

    优化逻辑联网逻辑

--- net.ps1
+++ net.ps1
@@ -1,12 +1,18 @@
+$count=0
 while(1){
     $x=ping -n 1 baidu.com
-    sleep 1
     if($x[5][30] -eq "0"){
-        echo "Already connected"
-        sleep 3
+        $count=$count+1
+        echo "Already connected,count=$count"
+        sleep 1
     }else{
-        echo "using scunet script to log in..."
-        scunet
-        echo "done"
+        do{
+	echo "using scunet script to log in..."
+	scunet
+	echo "done"
+	sleep 1
+	$x=ping -n 1 baidu.com
+        }while($x[5][30] -eq "1");
+        $count=0
     }
 }
